e66a645f646796e6e0a416efcfc2680f710b2017
Nel4Nelson <an.ekwueme@stu.unizik.edu.ng>
2024-08-06 16:53:03 +0100

    initial commit
    Next js set up and implementation of navbar fully responsive

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import NavBar from "@/components/LandingPage/NavBar";
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <section className="border-b border-solid border-nav-border">
+        <NavBar />
+      </section>
+    </div>
+  );
+};
+
+export default LandingPage;
