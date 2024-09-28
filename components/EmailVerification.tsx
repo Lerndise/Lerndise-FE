7658060831a851b722e020190eb08360226984c2
Nel4Nelson <an.ekwueme@stu.unizik.edu.ng>
2024-09-28 13:29:55 +0100

    minor fix

--- components/EmailVerification.tsx
+++ components/EmailVerification.tsx
@@ -20,7 +20,7 @@ const EmailVerification: React.FC = () => {
       }
 
       try {
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/verify-email`, {
+        const response = await axios.get(`https://backend-learndise.azurewebsites.net/verify-email`, {
           params: { code }
         });
 
