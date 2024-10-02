39f75e1b3ded37af1b484422aa30ce83ed92e357
Pascal Mbagwu <your.email@example.com>
2024-10-02 15:34:33 +0100

    feat(AdminSidenav): Designed the admin sidebar

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,9 +1,12 @@
+import SideNav from '@/components/DashboardPage/SideNav';
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
-  )
+    <div className='flex w-full items-start bg-white-a700'>
+      <SideNav />
+    </div>
+  );
 }
 
 export default page
