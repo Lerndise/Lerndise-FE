cf4d0818c3a0041789618321094a3a65cc82f3ec
Pascal Mbagwu <your.email@example.com>
2024-10-02 15:24:35 +0100

    feat(AdminSidenav): Designed the admin sidebar

---
+++ app/admindashboard/page.tsx
@@ -0,0 +1,10 @@
+import SideNav from "@/components/DashboardPage/SideNav";
+
+const Home = () => {
+  return (
+    <div className="flex w-full items-start bg-white-a700">
+      <SideNav />
+    </div>
+  );
+};
+export default Home;
